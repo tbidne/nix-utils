98138380f7d09aafbd0074c156c27f1ab4ffb936
Tommy Bidne <tbidne@protonmail.com>
2022-08-03 23:07:31 +1200

    Cleanup test

--- test/pkgs-hash-or-flake.nix
+++ test/pkgs-hash-or-flake.nix
@@ -1,15 +1,17 @@
 { test ? "hash" }:
 
 let
-  pkgs-hash = (import ../default.nix).pkgs-hash-or-flake { hash = "b39924fc7764c08ae3b51beef9a3518c414cdb7d"; };
-  pkgs-flake = (import ../default.nix).pkgs-hash-or-flake { flake-path = ./test-flake.lock; };
+  pkgs-hash = { hash = "b39924fc7764c08ae3b51beef9a3518c414cdb7d"; };
+  pkgs-flake = { flake-path = ./test-flake.lock; };
 
-  pkgs =
+  input =
     if test == "hash"
     then pkgs-hash
     else
       if test == "flake"
       then pkgs-flake
       else throw "Expected 'hash' or 'flake' for arg 'test', received ${test}";
+
+  pkgs = (import ../default.nix).pkgs-hash-or-flake input;
 in
 pkgs.mkShell { }
