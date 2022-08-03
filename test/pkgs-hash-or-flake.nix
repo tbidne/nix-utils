c6ba5da91122d0f89a4e37cbe1e796f6be3c9317
Tommy Bidne <tbidne@protonmail.com>
2022-08-04 00:11:34 +1200

    Improve tests

--- test/pkgs-hash-or-flake.nix
+++ test/pkgs-hash-or-flake.nix
@@ -1,16 +1,13 @@
-{ test ? "hash" }:
+{ use-hash ? true }:
 
 let
   pkgs-hash = { hash = "b39924fc7764c08ae3b51beef9a3518c414cdb7d"; };
   pkgs-flake = { flake-path = ./test-flake.lock; };
 
   input =
-    if test == "hash"
+    if use-hash
     then pkgs-hash
-    else
-      if test == "flake"
-      then pkgs-flake
-      else throw "Expected 'hash' or 'flake' for arg 'test', received ${test}";
+    else pkgs-flake;
 
   pkgs = (import ../default.nix).pkgs-hash-or-flake input;
 in
